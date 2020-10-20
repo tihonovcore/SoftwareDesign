b6ab989ecea9c20782dc771375ff26d680dc2bd5
Vitaly <tihonovcore@gmail.com>
2020-10-20 15:07:02 +0300

    [hw3-refactor] extract similar code

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -2,59 +2,71 @@ package ru.akirakozov.sd.refactoring.servlet;
 
 import kotlin.Pair;
 import ru.akirakozov.sd.refactoring.HtmlWriter;
-import ru.akirakozov.sd.refactoring.SqlFacade;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author akirakozov
  */
 public class QueryServlet extends AbstractProductServlet {
+    private final List<String> commands = Arrays.asList("max", "min", "count", "sum");
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String command = request.getParameter("command");
         HtmlWriter htmlWriter = new HtmlWriter(response.getWriter());
 
-        if ("max".equals(command)) {
-            var products = readAll("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-
+        if (commands.contains(command)) {
             htmlWriter.start();
-            htmlWriter.h1("Product with max price: ");
-            for (Pair<String, Integer> product : products) {
-                htmlWriter.print(product.getFirst() + "\t" + product.getSecond());
-                htmlWriter.br();
+
+            switch (command) {
+                case "max": {
+                    var products = readAll("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
+
+                    htmlWriter.h1("Product with max price: ");
+                    for (Pair<String, Integer> product : products) {
+                        htmlWriter.print(product.getFirst() + "\t" + product.getSecond());
+                        htmlWriter.br();
+                    }
+
+                    break;
+                }
+                case "min" : {
+                    var products = readAll("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
+
+                    htmlWriter.h1("Product with min price: ");
+                    for (Pair<String, Integer> product : products) {
+                        htmlWriter.print(product.getFirst() + "\t" + product.getSecond());
+                        htmlWriter.br();
+                    }
+
+                    break;
+                }
+                case "sum" : {
+                    var sum = readInt("SELECT SUM(price) FROM PRODUCT");
+
+                    htmlWriter.println("Summary price: ");
+                    if (sum != null) htmlWriter.println(sum);
+
+                    break;
+                }
+                case "count" : {
+                    var count = readInt("SELECT COUNT(*) FROM PRODUCT");
+
+                    htmlWriter.println("Number of products: ");
+                    if (count != null) htmlWriter.println(count);
+
+                    break;
+                }
             }
-            htmlWriter.end();
-        } else if ("min".equals(command)) {
-            var products = readAll("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
 
-            htmlWriter.start();
-            htmlWriter.h1("Product with min price: ");
-
-            for (Pair<String, Integer> product : products) {
-                htmlWriter.print(product.getFirst() + "\t" + product.getSecond());
-                htmlWriter.br();
-            }
-            htmlWriter.end();
-        } else if ("sum".equals(command)) {
-            var sum = readInt("SELECT SUM(price) FROM PRODUCT");
-
-            htmlWriter.start();
-            htmlWriter.println("Summary price: ");
-            if (sum != null) htmlWriter.println(sum);
-            htmlWriter.end();
-        } else if ("count".equals(command)) {
-            var count = readInt("SELECT COUNT(*) FROM PRODUCT");
-
-            htmlWriter.start();
-            htmlWriter.println("Number of products: ");
-            if (count != null) htmlWriter.println(count);
             htmlWriter.end();
         } else {
-            response.getWriter().println("Unknown command: " + command);
+            htmlWriter.println("Unknown command: " + command);
         }
 
         response.setContentType("text/html");
