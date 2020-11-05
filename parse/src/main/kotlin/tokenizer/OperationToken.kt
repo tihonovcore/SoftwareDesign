3b471ab434113c173b2fe88de4daddeca8127030
Vitaly <tihonovcore@gmail.com>
2020-11-06 01:13:50 +0300

    [hw6-parser] add priorities to operations

--- parse/src/main/kotlin/tokenizer/OperationToken.kt
+++ parse/src/main/kotlin/tokenizer/OperationToken.kt
@@ -3,6 +3,8 @@ package tokenizer
 import visitors.TokenVisitor
 
 abstract class OperationToken : Token {
+    abstract val priority: Int
+
     override fun accept(visitor: TokenVisitor) {
         visitor.visit(this)
     }
@@ -11,23 +13,32 @@ abstract class OperationToken : Token {
 data class Plus(
     override val text: String,
     override val from: Int,
-    override val to: Int
-) : OperationToken()
+    override val to: Int,
+
+) : OperationToken()  {
+    override val priority: Int = 0
+}
 
 data class Subtract(
     override val text: String,
     override val from: Int,
     override val to: Int
-) : OperationToken()
+) : OperationToken() {
+    override val priority: Int = 0
+}
 
 data class Multiply(
     override val text: String,
     override val from: Int,
     override val to: Int
-) : OperationToken()
+) : OperationToken() {
+    override val priority: Int = 1
+}
 
 data class Divide(
     override val text: String,
     override val from: Int,
     override val to: Int
-) : OperationToken()
+) : OperationToken() {
+    override val priority: Int = 1
+}
