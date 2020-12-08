a06b6c38609115f06d8b1320745b5cc23d3856a9
Vitaly <tihonovcore@gmail.com>
2020-12-08 10:15:57 +0300

    [hw7-aop] fix main, add TODOs

--- aop/src/main/java/profiler/Main.java
+++ aop/src/main/java/profiler/Main.java
@@ -7,18 +7,18 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        if (args == null || args.length != 3 || Arrays.stream(args).anyMatch(Objects::isNull)) {
-//            System.out.println("Expected arguments: <package name> <path to package> <path to save statistics>");
-//            return;
-//        }
+        if (args == null || args.length != 3 || Arrays.stream(args).anyMatch(Objects::isNull)) {
+            System.out.println("Expected arguments: <package name> <path to package> <path to save statistics>");
+            return;
+        }
 
-//        String packageName = args[0];
-//        String pathToPackage = args[1];
-//        String pathToStatistics = args[2];
+        String packageName = args[0];
+        String pathToPackage = args[1];
+        String pathToStatistics = args[2];
 
-        String packageName = "bank";
-        String pathToPackage = "/home/tihonovcore/IdeaProjects/SoftwareDesign/aop/src/main/java/";
-        String pathToStatistics = "/home/tihonovcore/IdeaProjects/SoftwareDesign/aop/target/statistics.txt";
+//        String packageName = "bank";
+//        String pathToPackage = "/home/tihonovcore/IdeaProjects/SoftwareDesign/aop/src/main/java/";
+//        String pathToStatistics = "/home/tihonovcore/IdeaProjects/SoftwareDesign/aop/target/statistics.txt";
 
         String pathToProfiler = System.getProperty("user.dir") + "/aop/src/main/resources/AspectProfiler.java";
 
@@ -33,7 +33,8 @@ public class Main {
     ) throws IOException {
         copyProfiler(pathToPackage, pathToProfiler, pathToStatistics, packageName);
         compile();
-        removeProfiler(pathToPackage);
+        // TODO: need to remove copy of profiler, but ajc exit from application before it
+        removeProfiler(pathToPackage, packageName);
     }
 
     private void copyProfiler(
@@ -61,13 +62,14 @@ public class Main {
     }
 
     private void compile() throws IOException {
+        //TODO: use relative paths
         String path = "/home/tihonovcore/IdeaProjects/SoftwareDesign/aop/src/main/java/bank";
         String source = "/home/tihonovcore/.m2/repository/org/aspectj/aspectjrt/1.9.6/aspectjrt-1.9.6.jar";
         String dest = "/home/tihonovcore/IdeaProjects/SoftwareDesign/aop/target";
         org.aspectj.tools.ajc.Main.main(new String[]{"-1.8", "-cp", source, "-d", dest, "-sourceroots", path});
     }
 
-    private void removeProfiler(String pathToPackage) throws IOException {
-        Files.delete(Paths.get(pathToPackage + "/AspectProfiler.java"));
+    private void removeProfiler(String pathToPackage, String packageName) throws IOException {
+        Files.delete(Paths.get(pathToPackage + packageName.replace('.', '/') + "/AspectProfiler.java"));
     }
 }
